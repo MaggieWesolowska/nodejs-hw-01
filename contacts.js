c7f0cdcc6dc257d08e04144e1726cfbcb9837abd
MaggieWesolowska <maggiewes07@gmail.com>
2023-07-27 14:44:54 +0200

    edited js files to add functionality

--- contacts.js
+++ contacts.js
@@ -10,3 +10,51 @@ const contactsPath = path.join(
 );
 
 console.log(contactsPath);
+
+const updateContacts = async contacts =>
+  await fs.writeFile(
+    contactsPath,
+    JSON.stringify(contacts, null, 2)
+  );
+
+const listContacts = async () => {
+  const result = await fs.readFile(contactsPath);
+  return JSON.parse(result);
+};
+
+const getContactById = async id => {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === id);
+  return result || null;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+const removeContact = async id => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
